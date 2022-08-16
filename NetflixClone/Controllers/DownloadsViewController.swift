e24a6b09efcdfc507a294084bbb437af499e8a55
andrei <kvasha007@mail.ru>
2022-08-16 14:45:25 +0300

    Initial Commit

---
+++ NetflixClone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ NetflixClone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
